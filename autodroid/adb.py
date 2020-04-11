be0e44cf7624eeea754632bddcad23429a2c8200
zhujinchang <stdzhu@gmail.com>
2020-04-11 15:07:36 +0800

    Add some useful functions.

--- autodroid/adb.py
+++ autodroid/adb.py
@@ -1,11 +1,26 @@
 from subprocess import check_output
 import os
+import shutil
 
-adb_command = "adb"
+adb_command = None
+
+physical_size_leading = "Physical size: "
+override_size_leading = "Override size: "
+physical_density_leading = "Physical density: "
+override_density_leading = "Physical density: "
 
 
 def init_adb():
-    pass
+    global adb_command
+    if shutil.which("adb") is not None:
+        adb_command = "adb"
+    else:
+        android_home = os.environ.get("ANDROID_HOME")
+        if android_home is not None:
+            adb_command = os.path.join(android_home, "platform-tools", "adb")
+
+    if adb_command is None:
+        raise Exception("No adb command found!")
 
 
 def cap_screen_pic():
@@ -14,3 +29,41 @@ def cap_screen_pic():
 
 def click_point(x, y):
     os.system(f'{adb_command} shell input tap {x} {y}')
+
+
+def start_activity(activity):
+    """
+    activity: package name + activity name, like: com.tencent.mm/com.tencent.mm.ui.LauncherUI
+    """
+    os.system(f'{adb_command} shell am start -n {activity}')
+
+
+def get_screen_size(in_dp=False):
+    raw_output = check_output(f'{adb_command} shell wm size')
+    raw_output = str(raw_output, encoding='utf-8')
+    output_lines = raw_output.splitlines()
+    output_lines = sorted(output_lines)
+    sizes = output_lines[0].split(":")[1].strip().split("x")
+    sizes = (int(sizes[0]), int(sizes[1]))
+    if not in_dp:
+        return sizes
+    else:
+        density = get_density()
+        width_dp = 480 / density * 360
+        height_dp = sizes[1] / sizes[0] * width_dp
+        return (round(width_dp), round(height_dp))
+
+
+def get_density():
+    raw_output = check_output(f'{adb_command} shell wm density')
+    raw_output = str(raw_output, encoding='utf-8')
+    output_lines = raw_output.splitlines()
+    output_lines = sorted(output_lines)
+    density = output_lines[0].split(":")[1].strip()
+    return int(density)
+
+
+def get_dpi():
+    size_in_px = get_screen_size(in_dp=False)
+    size_in_dp = get_screen_size(in_dp=True)
+    return size_in_px[0] / size_in_dp[0]
--- autodroid/tools.py
+++ autodroid/tools.py
@@ -1,14 +1,66 @@
-from subprocess import check_output
-
-import numpy as np
 import cv2
+import numpy as np
 
 import autodroid.adb as adb
 
 
-def fetch_screen_img():
+def fetch_screen_img(scale_to_dp=False):
     png_raw = adb.cap_screen_pic()
     png_raw = np.asarray(bytearray(png_raw), dtype=np.uint8)
     img = cv2.imdecode(png_raw, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    if scale_to_dp:
+        img = cv2.resize(img, adb.get_screen_size(in_dp=True), interpolation=cv2.INTER_AREA)
+
     return img
+
+
+def match(img, templ_img, match_one=True, threshold=0.6, de_dup=True):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    templ_gray = cv2.cvtColor(templ_img, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.matchTemplate(img_gray, templ_gray, cv2.TM_CCOEFF_NORMED)
+
+    if match_one:
+        max = np.max(res)
+        loc = np.where(res == max)
+        x = loc[1][0]
+        y = loc[0][0]
+        return (x, y, x + templ_gray.shape[1], y + templ_gray.shape[0])
+
+    loc = np.where(res > threshold)
+    loc = sorted(zip(loc[0], loc[1]), key=lambda pos: -res[pos[0]][pos[1]])
+
+    matches = list()
+    for y, x in loc:
+        matches.append((x, y, x + templ_gray.shape[1], y + templ_gray.shape[0]))
+
+    if de_dup:
+        no_dup = list()
+        for item in matches:
+            if len(no_dup) == 0:
+                no_dup.append(item)
+            else:
+                distance = item[0] - no_dup[-1][0]
+                if distance < -5 or distance > 5:
+                    no_dup.append(item)
+    else:
+        no_dup = matches
+
+    return no_dup
+
+
+def scale_image(origin_image, factor):
+    """
+    To shrink an image, it will generally look best with cv::INTER_AREA interpolation, 
+    whereas to enlarge an image, it will generally look best with cv::INTER_CUBIC (slow) or cv::INTER_LINEAR (faster but still looks OK).
+    """
+    if factor == 1:
+        return origin_image
+
+    if factor > 1:
+        inter = cv2.INTER_CUBIC
+    else:
+        inter = cv2.INTER_AREA
+    return cv2.resize(origin_image, (0, 0), fx=factor, fy=factor, interpolation=inter)
